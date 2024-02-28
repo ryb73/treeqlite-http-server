13ba7a37fcd6349e00d3f5b57b96e90d63559cf6
Ryan <rpbiwer@gmail.com>
2024-02-28 14:17:24 -0500

    Listen on random port for testing

--- routes/index.test.ts
+++ routes/index.test.ts
@@ -1,23 +1,28 @@
 import { createServer } from "http";
 // eslint-disable-next-line @typescript-eslint/no-shadow
-import { afterAll, beforeAll, test } from "vitest";
+import { afterAll, beforeAll, test, assert } from "vitest";
+import { defined } from "@ryb73/super-duper-parakeet/lib/src/type-checks";
 import app from "../app";
-
-app.set(`port`, 3000);
 
 const server = createServer(app);
 
 beforeAll(() => {
-  server.listen(3000);
+  server.listen(0);
 });
 
 afterAll(() => {
   server.close();
 });
 
+function getPort() {
+  const address = defined(server.address());
+  assert(typeof address === "object");
+  return address.port;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-shadow
 test(`index`, async ({ expect }) => {
-  const response = await fetch(`http://localhost:3000`);
+  const response = await fetch(`http://localhost:${getPort()}`);
   const text = await response.text();
   expect(text).toMatchInlineSnapshot(`"Hello World!"`);
   expect(response.headers.get(`content-type`)).toMatchInlineSnapshot(
